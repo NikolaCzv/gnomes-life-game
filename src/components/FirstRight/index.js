c90d26e70d0da9df7cd1aa1b8224440cebd5fffc
Rabbitosa <Rabbitosa2003@gmail.com>
2020-09-10 22:33:41 +0200

    snake left and right

--- src/components/FirstRight/index.js
+++ src/components/FirstRight/index.js
@@ -1,53 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, QuestText, ButtonWrapper, Health } from '../style';
 import Button from '@material-ui/core/Button';
 
-
-const FirstRight = props => {
-    const [isGently, setIsGently] = useState(false);
-    const [isStone, setIsStone] = useState(false);
-    const [isScreem, setIsScreem] = useState(false);
-
-    const handleGentlyButton = () => setIsGently(true);
-    const handleStoneButton = () => setIsStone(true);
-    const handleScreemButton = () => setIsScreem(true);
-
-    if(isGently)
-    return
-
+const FirstRight = (props) => {
  return(  
     <Container>
         <Health>
-            Gnome's Energy: {props.energy} ❤️
+            Energy: {props.energy} 
         </Health>
         <QuestText>
-            OWL SLEEPING
+            FirstRight
+            
         </QuestText>
         <ButtonWrapper>
-            <Button
-                variant="contained" 
-                color="primary"
-                onClick={handleGentlyButton}
-            >
-                Gently
-            </Button>
-            <Button
-                variant="contained" 
-                color="primary"
-                onClick={handleStoneButton}
-            >
-                Try a stone at the owl
-            </Button>
-            <Button
-                variant="contained" 
-                color="primary"
-                onClick={handleScreemButton}
-            >
-                Screem
-            </Button>
         </ButtonWrapper>
     </Container>
  );
 };
+
 
 export default FirstRight;
