af14303032383af456d86df0d4d385ed470c4ec3
github NikolaCzv <nikola.raicic@hotmail.com>
2020-09-10 14:56:33 -0500

    minor issue fixed

--- src/components/FirstRight/index.js
+++ src/components/FirstRight/index.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, QuestText, ButtonWrapper, Health } from '../style';
+import Button from '@material-ui/core/Button';
 
 
 const FirstRight = props => {
+    const [isGently, setIsGently] = useState(false);
+    const [isStone, setIsStone] = useState(false);
+    const [isScreem, setIsScreem] = useState(false);
+
+    const handleGentlyButton = () => setIsGently(true);
+    const handleStoneButton = () => setIsStone(true);
+    const handleScreemButton = () => setIsScreem(true);
+
+    if(isGently)
+    return
+
  return(  
     <Container>
         <Health>
             Gnome's Energy: {props.energy} ❤️
         </Health>
         <QuestText>
-            FirstRight
-            
+            OWL SLEEPING
         </QuestText>
         <ButtonWrapper>
+            <Button
+                variant="contained" 
+                color="primary"
+                onClick={handleGentlyButton}
+            >
+                Gently
+            </Button>
+            <Button
+                variant="contained" 
+                color="primary"
+                onClick={handleStoneButton}
+            >
+                Try a stone at the owl
+            </Button>
+            <Button
+                variant="contained" 
+                color="primary"
+                onClick={handleScreemButton}
+            >
+                Screem
+            </Button>
         </ButtonWrapper>
     </Container>
  );
